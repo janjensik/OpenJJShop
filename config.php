91b1771a5cddc22633d1583f64acc9950ff1d619
Orbis <Orbis@10.0.0.35>
2012-06-05 16:51:30 +0200

    fix: stromová struktura

--- config.php
+++ config.php
@@ -2,16 +2,59 @@
 
 $img_dir = "img";
 
-$menu = array("Home" => "index.php", "O nás" => "aboutus.php", "Bloky" => "blocks.php", "Tabulka" => "table.php");
+$menu = array("Home" => "index.php",
+              "O nás" => "aboutus.php",
+              "Bloky" => "blocks.php",
+              "Tabulka" => "table.php");
 
-$zbozi = array(array("nazev" => "Sada SRAM Red", "imgfile" => "red.jpg", "popis" => "Nový SRAM RED představuje vrcholnou technologii v silniční cyklistice a přináší nejlepší možnou technologii 10 rychlostního řazení s extra nízkou hmotností - celá sada má pohých 1739 gramů!", "cena" => "43900 Kč"),
-               array("nazev" => "Sada Dura Ace 7900", "imgfile" => "duraace.jpg", "popis" => "Kompletně přepracované sada Dura Ace", "cena" => "33900 Kč"),
-               array("nazev" => "Sada Shimano Dura Ace Di2 ", "imgfile" => "duradi2.jpg", "popis" => "Elektronické komponenty Dura Ace Di2 se dostávají po několika letech testování do popředí zájmu. Digitálně řízená sada má aerodynamický design a nabízí vysoký výkon přesného a rychlého řazení pomocí mikrotlačítek na tradičním místě STI pák.", "cena" => "43900 Kč"),
-               array("nazev" => "Sada Shimano Ultegra Di2", "imgfile" => "ultegradi2.jpg", "popis" => "Elektronická verze je nyní k dispozici i u sady Ultegra. Digitální ovládání vylučuje jakoukoli nejistotu při řazení, omezuje riziko chyb lidského faktoru a umožňuje se plně koncetrovat na výkon. Přesmykač se automaticky dolaďuje podle zařazeného převodu vzadu, elektronický systém Di2 je prověřen při nejnáročnějších závodech a stal se dalším mezníkem úrovně jízdních vlastností.", "cena" => "26900  Kč"),
-               array("nazev" => "Sada Campagnolo Super Record", "imgfile" => "record.jpg", "popis" => "Sada představující jeden z vrcholů světa silniční cyklistiky. Naleznete v ní ty nejlehčí a nejtužší komponenty, které jsou vyrobeny z karbonu, titanu a hliníku. Ložiska jednotlivých komponentů (přehazovačky, řazení, klik a brzdových čelistí) pocházejí z řady keramických ložisek CULT patentovanými firmou Campagnolo. Hlavní novinkou této sady pro rok 2011 je celokarbonová přehazovačka. Jako na všechny ostatní sady z řady 11s i na sadu SUPER RECORD firma Campagnolo poskytuje 4-letou záruční dobu.", "cena" => "47900  Kč"),
-               array("nazev" => "Sada Ultegra 6700", "imgfile" => "ultegra.jpg", "popis" => "Novinková Shimano Ultegra navazuje designem na Dura Ace a přináší další snížení hmotnosti této oblíbené sady.", "cena" => "16900 Kč"),
-               array("nazev" => "Sada SRAM X0 RED", "imgfile" => "x0red.jpg", "popis" => "Sada lehčí, než Shimano XTR, je hitem firmy SRAM. Ve variantě se dvěmi převodníky je určena pro XC a maratonské závodní použití.", "cena" => "22450 Kč"),
-               array("nazev" => "Sada Shimano XT Dyna Sys 10sp bez brzd", "imgfile" => "xt.jpg", "popis" => "Novinková sada Shimano XT využívá systém Dyna Sys, který kromě 10 rychlostní kazety přináší lepší efektivitu přenosu síly. Kazeta s 34 nebo 36 zuby snižuje nutnost použití malého převodníku a tím dochází k menšímu křížení řetězu i jeho nižšímu namáhání.", "cena" => "8900 Kč"),
+$zbozi = array(
+             array(
+                 "nazev" => "Sada SRAM Red",
+                 "imgfile" => "red.jpg",
+                 "popis" => "Nový SRAM RED představuje vrcholnou technologii v silniční cyklistice a přináší nejlepší možnou technologii 10 rychlostního řazení s extra nízkou hmotností - celá sada má pohých 1739 gramů!",
+                 "cena" => "43900 Kč"
+                  ),
+             array(
+                 "nazev" => "Sada Dura Ace 7900",
+                 "imgfile" => "duraace.jpg", 
+                 "popis" => "Kompletně přepracované sada Dura Ace",
+                 "cena" => "33900 Kč"
+                  ),
+             array(
+                 "nazev" => "Sada Shimano Dura Ace Di2 ",
+                 "imgfile" => "duradi2.jpg",
+                 "popis" => "Elektronické komponenty Dura Ace Di2 se dostávají po několika letech testování do popředí zájmu. Digitálně řízená sada má aerodynamický design a nabízí vysoký výkon přesného a rychlého řazení pomocí mikrotlačítek na tradičním místě STI pák.",
+                 "cena" => "43900 Kč"
+                  ),
+             array(
+                 "nazev" => "Sada Shimano Ultegra Di2",
+                 "imgfile" => "ultegradi2.jpg",
+                 "popis" => "Elektronická verze je nyní k dispozici i u sady Ultegra. Digitální ovládání vylučuje jakoukoli nejistotu při řazení, omezuje riziko chyb lidského faktoru a umožňuje se plně koncetrovat na výkon. Přesmykač se automaticky dolaďuje podle zařazeného převodu vzadu, elektronický systém Di2 je prověřen při nejnáročnějších závodech a stal se dalším mezníkem úrovně jízdních vlastností.",
+                 "cena" => "26900  Kč"
+                  ),
+             array(
+                 "nazev" => "Sada Campagnolo Super Record",
+                 "imgfile" => "record.jpg",
+                 "popis" => "Sada představující jeden z vrcholů světa silniční cyklistiky. Naleznete v ní ty nejlehčí a nejtužší komponenty, které jsou vyrobeny z karbonu, titanu a hliníku. Ložiska jednotlivých komponentů (přehazovačky, řazení, klik a brzdových čelistí) pocházejí z řady keramických ložisek CULT patentovanými firmou Campagnolo. Hlavní novinkou této sady pro rok 2011 je celokarbonová přehazovačka. Jako na všechny ostatní sady z řady 11s i na sadu SUPER RECORD firma Campagnolo poskytuje 4-letou záruční dobu.",
+                 "cena" => "47900  Kč"
+                 ),
+             array(
+                 "nazev" => "Sada Ultegra 6700",
+                 "imgfile" => "ultegra.jpg",
+                 "popis" => "Novinková Shimano Ultegra navazuje designem na Dura Ace a přináší další snížení hmotnosti této oblíbené sady.",
+                 "cena" => "16900 Kč"
+                 ),
+             array(
+                 "nazev" => "Sada SRAM X0 RED",
+                 "imgfile" => "x0red.jpg",
+                 "popis" => "Sada lehčí, než Shimano XTR, je hitem firmy SRAM. Ve variantě se dvěmi převodníky je určena pro XC a maratonské závodní použití.",
+                 "cena" => "22450 Kč"
+                 ),
+             array(
+                 "nazev" => "Sada Shimano XT Dyna Sys 10sp bez brzd",
+                 "imgfile" => "xt.jpg",
+                 "popis" => "Novinková sada Shimano XT využívá systém Dyna Sys, který kromě 10 rychlostní kazety přináší lepší efektivitu přenosu síly. Kazeta s 34 nebo 36 zuby snižuje nutnost použití malého převodníku a tím dochází k menšímu křížení řetězu i jeho nižšímu namáhání.",
+                 "cena" => "8900 Kč"
+                 ),
         );           
-
 ?>
