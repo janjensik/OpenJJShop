247c7ee58d7561c064adacabe37e568454faf9c6
Orbis <Orbis@10.0.0.35>
2012-06-05 14:58:14 +0200

    zbozi v blocks pomocí php

--- blocks.php
+++ blocks.php
@@ -12,17 +12,16 @@ and open the template in the editor.
         <link rel="stylesheet" type="text/css" href="style.css"/>      
     </head>
     <body>
-        <?php include ("header_menu.php");?>
+        <?php
+        include ("header_menu.php");
+        include ("config.php");
         
-        <div class="blok"><p><img src="img/red.jpg" width="256" height="192" alt="Sada SRAM Red"/></p><p>Sada SRAM Red 2012 43900 Kč</p></div>
-        <div class="blok"><p><img src="img/duraace.jpg" width="256" height="192" alt="Sada Dura Ace"/></p><p>Sada Dura Ace 7900 33900 Kč</p></div>
-        <div class="blok"><p><img src="img/duradi2.jpg" width="256" height="192" alt="Sada Shimano Dura Ace Di2"/></p><p>Sada Shimano Dura Ace Di2 43900 Kč</p></div>
-        <div class="blok"><p><img src="img/ultegradi2.jpg" width="256" height="192" alt="Sada Shimano Ultegra Di2"/></p><p>Sada Shimano Ultegra Di2 26900 Kč</p></div>
-        <div class="blok"><p><img src="img/record.jpg" width="256" height="192" alt="Sada Campagnolo Super Record"/></p><p>Sada Campagnolo Super Record 47900 Kč</p></div>
-        <div class="blok"><p><img src="img/ultegra.jpg" width="256" height="192" alt="Sada Ultegra"/></p><p>Sada Ultegra 6700 16900 Kč</p></div>
-        <div class="blok"><p><img src="img/x0red.jpg" width="256" height="192" alt="Sada SRAM X0"/></p><p>Sada SRAM X0 RED 2x10 22450 Kč</p></div>
-        <div class="blok"><p><img src="img/xt.jpg" width="256" height="192" alt="Sada Shimano XT Dyna Sys"/></p><p>Sada Shimano XT Dyna Sys 10sp bez brzd 8900 Kč</p></div>
-        <div style="clear: both"></div>
+        foreach ($zbozi as $sada){
+            echo "<div class=\"blok\"><p><img src=\"$img_dir/$sada[imgfile]\" width=\"256\" height=\"192\" alt=\"$sada[nazev]\"/></p><p>$sada[cena]</p></div>";
+        }
+        ;?>
+        
+       <div style="clear: both"></div>
        <?php include ("footer.php");?>
              
     </body>
