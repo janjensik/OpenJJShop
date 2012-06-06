4926438e676afc85de85cd4ec6e170785d20b353
Orbis <Orbis@10.0.0.35>
2012-06-06 22:01:18 +0200

    přidána ochrana proti vniknutí

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
+include ("config.php");
 
 if (!isset($_REQUEST['q'])) $_REQUEST['q'] = "home";
-include $_REQUEST['q'].".php";
-
+if (in_array($_REQUEST['q'],$menu)) {
+   include($_REQUEST['q'].".php");
+}
+ else {
+   include ("eror404.php"); 
+}
 ?>
