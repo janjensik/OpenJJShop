ea5a26d78b1d7087a4da2dbf5aba75051a445850
Orbis <Orbis@10.0.0.35>
2012-06-06 23:52:34 +0200

    logo nese odkaz na home

--- header_menu.php
+++ header_menu.php
@@ -1,6 +1,6 @@
 <?php include "config.php";?>
 
-<div style="padding-top:1px; height: 160px; background-color: #00AEED; border-radius: 10px;"><p style="margin-left: 250px; font-family: serif; font-size: 40px; color: white;">OpenJJShop</p>
+<div style="padding-top:1px; height: 160px; background-color: #00AEED; border-radius: 10px;"><p style="margin-left: 250px; font-family: serif; font-size: 40px; color: white;"><a href="index.php?q=home">OpenJJShop</a></p>
              
             <span style="margin-left: 30px;">           
             <?php
