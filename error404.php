c6905c72e29a797b74da8cb6681bf47926de1cc9
Orbis <Orbis@10.0.0.35>
2012-06-06 22:47:29 +0200

    hezčí errorpage

--- error404.php
+++ error404.php
@@ -1,6 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html 
+     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>error404</title>
+        <link rel="stylesheet" type="text/css" href="style.css"/>      
+    </head>
+    <body>
+        <?php
+        include ("header_menu.php");
+        include ("config.php");    
+        ;?>
+        <span><h3><p>Chyba 404</p><p>Stránka<i><?php echo " $_REQUEST[q] ";?></i>nenalezena</p></h3></span>
 
-<span><h1>chtěl bys viď? nic nebude!</h1></span>
- 
+        
+       <div style="clear: both"></div>
+       <?php include ("footer.php");?>
+             
+    </body>
+</html>
 
 
