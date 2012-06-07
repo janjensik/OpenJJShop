af46dd73035090090d76832e2d1687bfaf31f5da
Orbis <Orbis@10.0.0.35>
2012-06-07 15:18:37 +0200

    stylování do .css

--- header_menu.php
+++ header_menu.php
@@ -1,16 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html 
+     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>        
+        <link rel="stylesheet" type="text/css" href="style.css"/>      
+    </head>
+    <body>
 <?php include "config.php";?>
 
-<div style="padding-top:1px; height: 160px; background-color: #00AEED; border-radius: 10px;"><p style="margin-left: 250px; font-family: serif; font-size: 40px; color: white;"><a href="index.php">OpenJJShop</a></p>
+<div class="header"><p class="logo_design"><a class="logo"href="index.php">OpenJJShop</a></p>
              
             <span style="margin-left: 30px;">           
             <?php
 
 foreach ($menu as $key => $page) 
       { ?>
-        <span class="menu"><a href="<?php echo "index.php?q=$page";?>"><span class="menu_font"><?php echo $key;?></span></a></span> 
+        <span class="menu"><a class="menu_odkaz" href="<?php echo "index.php?q=$page";?>"><span class="menu_font"><?php echo $key;?></span></a></span> 
         <?php
       } 
 
 ?>
             </span>
         </div>
+        
+    </body>
+</html>
